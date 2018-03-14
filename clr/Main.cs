ea4b8652e7f8cd56de3cdddf00366b23d612f63c
Sarath <nsarathsoman.com>
2018-03-14 13:10:04 +0530

    CLR monodis example

---
+++ clr/Main.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClrExample
+{
+    public class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+        	Console.WriteLine(Message.GetMessage());
+        }
+    }
+}
---
+++ clr/Message.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClrExample
+{
+    public class Message
+    {
+        public static string GetMessage()
+        {
+        	return "Hello, World!";
+        }
+    }
+}
